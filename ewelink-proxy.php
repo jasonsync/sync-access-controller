ffe256ae79ce6bbd72597d01c6dc275c2fa29f6a
Jason <jason@syncgroup.co.za>
2022-02-07 18:52:08 +0200

    stuff

--- ewelink-proxy.php
+++ ewelink-proxy.php
@@ -1,17 +1,71 @@
 <?php
     header('Access-Control-Allow-Private-Network: true');
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: localhost');
+    header('Content-Type: application/json; charset=utf-8');
 
-    // TODO: AUTHENTICATION from php session (after user has logged in)
+    $json = file_get_contents('php://input');
+    $data_json = json_decode($json, true);
 
-    $ch = curl_init('http://localhost:8000/');
-    # Setup request to send json via POST.
-    $payload = json_encode(array( "customer"=> 'jason' ));
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-    # Return response instead of printing.
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    # Send request.
-    $result = curl_exec($ch);
-    curl_close($ch);
-    print_r($result);
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (strpos($_SERVER["CONTENT_TYPE"], "application/x-www-form-urlencoded") !== false) {
+            $data = $_POST;
+        } elseif (strpos($_SERVER["CONTENT_TYPE"], "application/json") !== false) {
+            $data = $data_json;
+        } else {
+            die("Request failed. Expected HTTP POST 'CONTENT_TYPE' to be 'application/x-www-form-urlencoded' OR 'application/json'.");
+        }
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $data = $_GET;
+    } else {
+        die("Request failed. Expected 'REQUEST_METHOD' to be 'POST' or 'GET'.");
+    }
+
+        if (!isset($data['email'])) {
+            echo(json_encode(array('error' => true, 'msg'=>'missing email')));
+            exit;
+        }
+        if (!isset($data['password'])) {
+            echo(json_encode(array('error' => true, 'msg'=>'missing password')));
+            exit;
+        }
+        if (!isset($data['region'])) {
+            echo(json_encode(array('error' => true, 'msg'=>'missing region')));
+            exit;
+        }
+        if (!isset($data['deviceId'])) {
+            echo(json_encode(array('error' => true, 'msg'=>'missing deviceId')));
+            exit;
+        }
+        if (!isset($data['action'])) {
+            echo(json_encode(array('error' => true, 'msg'=>'missing action')));
+            exit;
+        }
+
+        $email = $data['email'];
+        $password = $data['password'];
+        $region = $data['region'];
+        $deviceId = $data['deviceId'];
+        $action = $data['action'];
+
+        // TODO: AUTHENTICATION from php session (after user has logged in)
+
+        $ch = curl_init('http://localhost:9000/');
+        # Setup request to send json via POST.
+        $payload = json_encode(array( "email"=> $email,
+      "password"=>$password,
+      "region"=>$region,
+      "deviceId"=>$deviceId,
+      "action"=>$action
+    ));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        # Return response instead of printing.
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        # Send request.
+        $result = curl_exec($ch);
+        curl_close($ch);
+        // print_r($result);
+        echo json_encode($result);
+        // echo json_encode(array('test'=>'teeets'));
+        exit;
--- nodejs-controller/ipconfig-announcer.js
+++ nodejs-controller/ipconfig-announcer.js
@@ -1,27 +1,28 @@
 'use strict';
 
 const fetch = require('node-fetch');
+const ip = require("ip");
+
 
 const {
   networkInterfaces
 } = require('os');
 
-const config = require('./config.json');
+const config = require('../config.json');
 // override default config with any set arguments
 argsToConfig();
 
 var fork = require('child_process').fork;
 var child = fork('./script');
 
-setInterval(() => {
-  detectAndAnnounce();
-}, (config.refreshInterval * 1000));
 
 console.log("announce.js started...");
 console.log();
 console.log("############");
-console.log("Announcing this device's internal + external IP's to remote IP address mapper: " + config.announceURL + " every " + config.refreshInterval + " seconds");
+console.log("Announcing this device's internal + external IP's to remote IP address mapper: " + config.serverAPI + " every " + config.refreshInterval + " seconds");
 console.log("############");
+
+
 detectAndAnnounce();
 
 //do something when app is closing
@@ -47,75 +48,88 @@ process.on('uncaughtException', exitHandler.bind(null, {
   exit: true
 }));
 
-
-
 // =====================
 // ===   FUNCTIONS   ===
 // =====================
 
 function detectNetwork() {
-  const nets = networkInterfaces();
-  const results = Object.create(null); // Or just '{}', an empty object
-
-  for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-      if (net.family === 'IPv4' && !net.internal) {
-        if (!results[name]) {
-          results[name] = [];
-        }
-        results[name].push(net.address);
-      }
-    }
-  }
-
-  var ip = null;
-  var netInterface = null;
-  if (results['eth0']) {
-    netInterface = "eth0";
-    ip = results['eth0'][0];
-  } else if (results['Wi-Fi']) {
-    netInterface = "Wi-Fi";
-    ip = results['Wi-Fi'][0];
-  }
-
-  // save to config
-  config.host.network.ip = ip;
-  config.host.network.interface = netInterface;
-
+  /*
+  //   const nets = networkInterfaces();
+  //   const results = Object.create(null); // Or just '{}', an empty object
+  //
+  //   for (const name of Object.keys(nets)) {
+  //     for (const net of nets[name]) {
+  //       // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+  //       if (net.family === 'IPv4' && !net.internal) {
+  //         if (!results[name]) {
+  //           results[name] = [];
+  //         }
+  //         results[name].push(net.address);
+  //       }
+  //     }
+  //   }
+  // console.log(nets);
+  //
+  //   var ip = null;
+  //   var netInterface = null;
+  //   if (results['eth0']) {
+  //     netInterface = "eth0";
+  //     ip = results['eth0'][0];
+  //   } else if (results['Wi-Fi']) {
+  //     netInterface = "Wi-Fi";
+  //     ip = results['Wi-Fi'][0];
+  //   }
+  //
+  //   // save to config
+  //   config.host.network.ip = ip;
+  //   config.host.network.interface = netInterface;
+  */
+  config.host.network.ip = ip.address();
   return {
-    "ip": ip,
-    "interface": netInterface
+    "ip": ip
   };
 }
 
 async function announce() {
   const currentDatetimeString = getCurrentDatetimeString();
-  if (config.host.network.interface && config.host.network.ip) {
+  if (config.host.network.ip) {
     console.log();
     console.log("# # # " + currentDatetimeString + " # # #");
-    console.log("# Device's current LAN connection: " + config.host.network.interface + " (" + config.host.network.ip + ")");
+    console.log("# Device's current LAN connection: " + config.host.network.ip);
     console.log("# Announcing IPs to server...");
 
     const req_body = {
       "host": config.host
     };
-
-    const response = await fetch(config.announceURL, {
-      method: 'post',
-      body: JSON.stringify(req_body),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
     try {
-      const data = await response.json();
+      const ms = Date.now();
+      const response = await fetch(config.serverAPI, {
+        method: 'post',
+        body: JSON.stringify(req_body),
+        headers: {
+          'Content-Type': 'application/json',
+          'pragma': 'no-cache',
+          'Cache-Control': 'no-cache'
+        }
+      });
+      try {
+        const data = await response.json();
 
-      console.log('# response:');
-      console.log(data);
+        console.log('# response:');
+        console.log(data);
+      } catch (e) {
+        console.log('\x1b[31m%s\x1b[0m', e); //red font
+      } finally{
+        setTimeout(() => {
+          detectAndAnnounce();
+        }, (config.refreshInterval * 1000));
+      }
     } catch (e) {
-      console.log(e);
+      console.log('\x1b[31m%s\x1b[0m', e); //red font
+
     }
+
+
   } else {
     console.log();
     console.log("!!! " + currentDatetimeString);
@@ -153,7 +167,7 @@ function argsToConfig() {
 };
 
 function exitHandler(options, exitCode) {
-  if (options.cleanup) console.log('Performing cleanup, terminating ewelink child process...');
-  if (exitCode || exitCode === 0) console.log("exitCode: " + exitCode);
+  if (options.cleanup) console.log('\x1b[33m%s\x1b[0m', 'Performing cleanup, terminating child processes...'); //yellow font
+  if (exitCode || exitCode === 0) console.log('\x1b[1;37m\x1b[1;41m%s\x1b[0m', "exitCode: " + exitCode);
   if (options.exit) process.exit();
 }
